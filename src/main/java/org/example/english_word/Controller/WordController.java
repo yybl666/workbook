0267411ef865f09da7db9a66acee840b99c86ce5
yybl666 <1596809031@qq.com>
2026-05-16 11:16:06 +0800

    添加更新编辑功能

--- src/main/java/org/example/english_word/Controller/WordController.java
+++ src/main/java/org/example/english_word/Controller/WordController.java
@@ -27,4 +27,26 @@ public class WordController {
     public void delete(@PathVariable Long id) {
         wordRepository.deleteById(id);
     }
+
+    @PutMapping("/{id}")
+    public Word update(@PathVariable Long id, @RequestBody Word newWord) {
+        /*你传了id就是要根据id进行一个判断，就是这个对象存不存在。
+        * 除此之外，也是需要根据id得到指定对象，这个对象里有全面数据，这里参数newWord不一定
+        * 所有的成员变量都有更改数据，到时候是以null被处理了。*/
+        Word word = wordRepository.findById(id).orElse(null);
+
+        if (word == null) {
+            return null;
+        }
+
+        if(newWord.getWord()!=null){
+            word.setWord(newWord.getWord());
+        }
+
+        if(newWord.getMeaning()!=null){
+            word.setMeaning(newWord.getMeaning());
+        }
+
+        return wordRepository.save(word);
+    }
 }
